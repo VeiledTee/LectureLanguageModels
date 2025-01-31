eb84cb3a17b22c5216a75a794b0782a025e0bc40
penguins <ethanheavey@outlook.com>
2025-01-31 15:47:17 -0400

    formatting

--- Education RA/test.py
+++ Education RA/test.py
@@ -1,21 +1,24 @@
-import modal
 import os
 import subprocess
+
+import modal
 
 # Initialize the volume
 volume = modal.Volume.from_name("my-persisted-volume", create_if_missing=True)
 
 # List files in the '/root/' directory
-print(volume.listdir('/root/'))
+print(volume.listdir("/root/"))
 
 # Iterate through files in the '/root/' directory recursively
-for filename in volume.iterdir(path='/root/', recursive=True):
-    if filename.path.endswith('_answerless.txt'):
+for filename in volume.iterdir(path="/root/", recursive=True):
+    if filename.path.endswith("_answerless.txt"):
         print(f"Found file: {filename.path}")
 
         # Construct the local path where you want to save the file
-        local_path = os.path.join("/home/penguins/Documents/PhD/LectureLanguageModels/Education RA/AI Course",
-                                  os.path.basename(filename.path))
+        local_path = os.path.join(
+            "/home/penguins/Documents/PhD/LectureLanguageModels/Education RA/AI Course",
+            os.path.basename(filename.path),
+        )
 
         command = [
             "modal",
@@ -23,7 +26,7 @@ for filename in volume.iterdir(path='/root/', recursive=True):
             "get",
             "my-persisted-volume",
             filename.path,
-            local_path
+            local_path,
         ]
 
         # Run the command
